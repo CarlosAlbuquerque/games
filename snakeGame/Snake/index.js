249b335530356f59300e135f092f64ff60dd1339
Carlos <carloshalbuquerque2@gmail.com>
2022-03-16 15:53:48 -0300

    feat: alter snake speed

--- snakeGame/Snake/index.js
+++ snakeGame/Snake/index.js
@@ -2,7 +2,7 @@ import { getInputDirection } from './input.js'
 import { gameboard } from '../Board/index.js';
 
 // Velocidade da cobra
-export const snakeSpeed = 4;
+export const snakeSpeed = 10;
 
 let newSegment = 0;
 
@@ -10,7 +10,7 @@ const snakeBody = [
     { x: 11, y: 11 },
 ]
 
-export function update(){
+export function update() {
     addSegments()
     const inputDirection = getInputDirection();
 
@@ -18,7 +18,7 @@ export function update(){
     // loop do penultimo elemento até o primeiro
     for (let i = snakeBody.length - 2; i >= 0; i--) {
         //os ... vai dentro do elemento e traz tudo que tem dentro dele, x e o y
-        snakeBody[i + 1] = {...snakeBody[i]};
+        snakeBody[i + 1] = { ...snakeBody[i] };
     }
 
     // fazer mover a cabeça
@@ -26,25 +26,25 @@ export function update(){
     snakeBody[0].y += inputDirection.y;
 };
 
-export function draw(){
+export function draw() {
 
     snakeBody.forEach(segment => {
-    //criar o elemento 
-    const snakeElement = document.createElement('div');
+        //criar o elemento 
+        const snakeElement = document.createElement('div');
 
-    //config css element
-    snakeElement.classList.add('snake')
+        //config css element
+        snakeElement.classList.add('snake')
 
-    // posição
-    snakeElement.style.gridRowStart = segment.y;
-    snakeElement.style.gridColumnStart = segment.x;
+        // posição
+        snakeElement.style.gridRowStart = segment.y;
+        snakeElement.style.gridColumnStart = segment.x;
 
-    //colocar no DOM
+        //colocar no DOM
         gameboard.appendChild(snakeElement);
     })
 };
 
-export function collision(position){
+export function collision(position) {
     // O some vai retorna true se pelo menos uma das condições for positiva, mesmo havendo inumeras negativas.
     return snakeBody.some(segment => {
         // quando se a posição que eu receber do onbejto externo for igual 
@@ -54,12 +54,12 @@ export function collision(position){
 }
 
 // expandindo a cobra
-export function expandSnake(amount){
+export function expandSnake(amount) {
     newSegment += amount;
 }
 
-function addSegments(){
-    if(newSegment > 0){
+function addSegments() {
+    if (newSegment > 0) {
         snakeBody.push({
             //estamos pegando o ultimo segmento dentro do array e add um novo elemento
             ...snakeBody[snakeBody.length - 1],
@@ -72,16 +72,16 @@ function addSegments(){
 // auxiliar functions 
 
 // pegando a cabeça da snake do
-export function getSnakeHead(){
+export function getSnakeHead() {
     return snakeBody[0];
 }
 
-export function hasSelfCollision(){
+export function hasSelfCollision() {
     const snakeHead = snakeBody[0];
 
-        return snakeBody.some((segment, index) => {
-            if (index === 0) return false;
-            
-            return snakeHead.x === segment.x && snakeHead.y === segment.y;
-        })
+    return snakeBody.some((segment, index) => {
+        if (index === 0) return false;
+
+        return snakeHead.x === segment.x && snakeHead.y === segment.y;
+    })
 }
